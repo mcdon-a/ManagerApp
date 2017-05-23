17a31dd941687c68890443b2ee7d31953327343b
Alexie McDonald <alexiemcdonald304@gmail.com>
2017-05-23 15:40:20 -0400

    Import the new EMAIL_CHANGED variable

---
+++ manager/src/reducers/AuthReducer.js
@@ -0,0 +1,11 @@
+import { EMAIL_CHANGED } from '../actions/types';
+
+const INITIAL_STATE = { email: '' };
+export default (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        default:
+            return state;
+        case EMAIL_CHANGED:
+
+    }
+};
