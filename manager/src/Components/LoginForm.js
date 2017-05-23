f3b3b533d6edecb0bafb1122c0986e87fdb0ea6a
Alexie McDonald <alexiemcdonald304@gmail.com>
2017-05-23 15:22:36 -0400

    Disabled eslint error and imported emailchanged action

--- manager/src/Components/LoginForm.js
+++ manager/src/Components/LoginForm.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { emailChanged } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
+
 class LoginForm extends Component {
-    onEmailChange(text){
-        
+    onEmailChange(text) {
+        this.props.emailChanged(text);
     }
     
     render() {
@@ -25,4 +28,4 @@ class LoginForm extends Component {
     }
 }
 
-export default LoginForm;
+export default connect(null, { emailChanged })(LoginForm);
