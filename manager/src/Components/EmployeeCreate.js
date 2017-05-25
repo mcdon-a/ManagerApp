4980e37216d2b0875fba5c3fbe1470d5aafbedbd
Alexie McDonald <alexiemcdonald304@gmail.com>
2017-05-25 14:50:47 -0400

    Create inputs for name and phone

--- manager/src/Components/EmployeeCreate.js
+++ manager/src/Components/EmployeeCreate.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { CardSection, Input, Button, Card } from './common';
 
 class EmployeeCreate extends Component {
     render() {
         return (
-            <View>
-                <Text>Employee Form</Text>
-            </View>
+            <Card>
+                <CardSection>
+                    <Input
+                    label="Name"
+                    placeholder="Jane"
+                    />
+                </CardSection>
+
+                <CardSection>
+                    <Input
+                    label="Phone"
+                    placeholder="555-555-555"
+                    />
+                </CardSection>
+
+                <CardSection />
+
+                <CardSection>
+                    <Button>
+                        Create
+                    </Button>
+                </CardSection>
+            </Card>
         );
     }
 }
