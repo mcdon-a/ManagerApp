b832930d6493193ea91a9aab75b13241e35e62fb
Alexie McDonald <alexiemcdonald304@gmail.com>
2017-05-25 14:07:28 -0400

    Enable scene navigation on login_success

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -8,8 +8,8 @@ const RouterComponent = () => {
     return (
         //This may have to be an android specific value for margin top
         <Router sceneStyle={{ marginTop: 50 }}>
-            <Scene key="EmployeeList" component={EmployeeList} title="Employee List"  />
-            <Scene key="login" component={LoginForm} title="Login"  />
+            <Scene key="login" component={LoginForm} title="Login" />
+            <Scene key="EmployeeList" component={EmployeeList} title="Employee List" />
         </Router>
     );
 };
