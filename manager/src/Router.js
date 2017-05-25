9b5710c076a31a08569cbdf1d2de68fa83997bd1
Alexie McDonald <alexiemcdonald304@gmail.com>
2017-05-25 14:00:18 -0400

    Created the employee list screen

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './Components/LoginForm';
+import EmployeeList from './Components/EmployeeList';
 
 
 const RouterComponent = () => {
     return (
         //This may have to be an android specific value for margin top
         <Router sceneStyle={{ marginTop: 50 }}>
-            <Scene key="login" component={LoginForm} title="Login" />
+            <Scene key="EmployeeList" component={EmployeeList} title="Employee List"  />
+            <Scene key="login" component={LoginForm} title="Login"  />
         </Router>
     );
 };
