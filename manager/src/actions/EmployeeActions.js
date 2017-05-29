1b994fa8fdce5cc76ed288c58bbe822fc70653f6
Alexie McDonald <alexiemcdonald304@gmail.com>
2017-05-29 13:17:51 -0400

    Clearing the add form after editing

--- manager/src/actions/EmployeeActions.js
+++ manager/src/actions/EmployeeActions.js
@@ -3,7 +3,8 @@ import { Actions } from 'react-native-router-flux';
 import {
     EMPLOYEE_UPDATE,
     EMPLOYEE_CREATE,
-    EMPLOYEE_FETCH_SUCCESS
+    EMPLOYEE_FETCH_SUCCESS,
+    EMPLOYEE_SAVE_SUCCESS
 } from './types';
 
 export const EmployeeUpdate = ({ prop, value }) => {
@@ -38,10 +39,13 @@ export const employeesFetch = () => {
 
 export const employeeSave = ({ name, phone, shift, uid }) => {
     const { currentUser } = firebase.auth();
-    return () => {
+    return (dispatch) => {
         firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
         .set({ name, phone, shift })
-        .then(() => console.log('saved!'));
+        .then(() => {
+            dispatch({ type: EMPLOYEE_SAVE_SUCCESS });
+            Actions.EmployeeList({ type: 'reset' });
+        });
     };
 };
 
