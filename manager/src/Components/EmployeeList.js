43dae1467e0816ad47ad288bb280e175715b54c1
Alexie McDonald <alexiemcdonald304@gmail.com>
2017-05-29 11:05:36 -0400

    Installed lodash and created the ListItem component

--- manager/src/Components/EmployeeList.js
+++ manager/src/Components/EmployeeList.js
@@ -1,7 +1,9 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { ListView, View, Text } from 'react-native';
+import { ListView } from 'react-native';
 import { employeesFetch } from '../actions';
+import ListItem from './ListItem';
 
 class EmployeeList extends Component {
     componentWillMount() {
@@ -25,18 +27,27 @@ class EmployeeList extends Component {
         this.dataSource = ds.cloneWithRows(employees);
     }
 
+    renderRow(employee) {
+        return <ListItem employee={employee} />;
+    }
+
     render() {
+        console.log(this.props);
         return (
-            <View>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-            </View>
+            <ListView
+            enableEmptySections
+            dataSource={this.dataSource}
+            renderRow={this.renderRow}
+            />
         );
     }
 }
 
-export default connect(null, { employeesFetch })(EmployeeList);
+const mapStateToProps = state => {
+    const employees = _.map(state.employees, (val, uid) => {
+        return { ...val, uid }; //{ shift: Monday, name: 's, id: '1214232342' }
+    });
+    return { employees };
+};
+
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
