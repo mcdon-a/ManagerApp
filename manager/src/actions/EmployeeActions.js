c6030781e4cc1640c0808dfb68d002c4a9b17266
Alexie McDonald <alexiemcdonald304@gmail.com>
2017-05-29 09:57:10 -0400

    Create new employee reducer and created the employee_fetch_success

--- manager/src/actions/EmployeeActions.js
+++ manager/src/actions/EmployeeActions.js
@@ -2,7 +2,8 @@ import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
 import {
     EMPLOYEE_UPDATE,
-    EMPLOYEE_CREATE
+    EMPLOYEE_CREATE,
+    EMPLOYEE_FETCH_SUCCESS
 } from './types';
 
 export const EmployeeUpdate = ({ prop, value }) => {
@@ -25,3 +26,12 @@ export const employeeCreate = ({ name, phone, shift }) => {
     };
 };
 
+export const employeesFetch = () => {
+    const { currentUser } = firebase.auth();
+    return (dispatch) => {
+        firebase.database().ref(`/users/${currentUser.uid}/employees`)
+        .on('value', snapshot => {
+            dispatch({ type: EMPLOYEE_FETCH_SUCCESS, payload: snapshot.val() });
+        });
+    };
+};
